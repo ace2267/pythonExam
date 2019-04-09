54d3efdab71aa197ff0c1564e4ac88a0bb6aec41
ace2267 <ace22672@gmail.com>
2019-04-09 18:00:04 +0900

    aaaa

---
+++ exam/condEx.py
@@ -0,0 +1,12 @@
+import sys
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+
+x = 10
+if x < 10:
+    print('한자리수')
+elif x < 100:
+    print('한자리수')
+    print("두자리수")
+else:
+    print("세자리이상")
